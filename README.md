68dc07600873645195eff5b378e887a89526d023
Sohinee <sc5248@srmist.edu.in>
2026-08-03 00:51:51 +0530

    Completed Week 1 Java Assignment

---
+++ README.md
@@ -0,0 +1,22 @@
+# Week 1 Assignment
+
+## Problems
+
+1. Seat Duplication Checker
+2. Typing Accuracy Checker
+3. Traffic Signal Analyzer
+4. Warehouse Inventory Balancer
+5. Movie Review Word Length Profiler
+
+## Language Used
+
+Java
+
+## Concepts Used
+
+- Arrays
+- Strings
+- Loops
+- Nested Loops
+- Conditional Statements
+- Methods
---
+++ WarehouseInventoryBalancer.java
@@ -0,0 +1,61 @@
+public class WarehouseInventoryBalancer {
+
+    public static void analyzeInventory(int[] sectionA, int[] sectionB) {
+
+        int totalA = 0;
+        int totalB = 0;
+
+        int highest = Integer.MIN_VALUE;
+        int index = 0;
+        String section = "";
+
+        for (int i = 0; i < sectionA.length; i++) {
+
+            totalA += sectionA[i];
+
+            if (sectionA[i] > highest) {
+
+                highest = sectionA[i];
+                section = "Section A";
+                index = i;
+            }
+        }
+
+        for (int i = 0; i < sectionB.length; i++) {
+
+            totalB += sectionB[i];
+
+            if (sectionB[i] > highest) {
+
+                highest = sectionB[i];
+                section = "Section B";
+                index = i;
+            }
+        }
+
+        if (totalA == totalB) {
+
+            System.out.println("Balanced");
+
+        } else {
+
+            System.out.println("Not Balanced");
+        }
+
+        System.out.println(
+                "Highest Quantity: "
+                        + highest
+                        + " in "
+                        + section
+                        + " at position "
+                        + (index + 1));
+    }
+
+    public static void main(String[] args) {
+
+        int[] sectionA = {20, 15, 30};
+        int[] sectionB = {25, 10, 30};
+
+        analyzeInventory(sectionA, sectionB);
+    }
+}
