35c7265b94059bffcd9d1e7a25595335b5e78f5c
Sohinee1 <sc5248@srmist.edu.in>
2026-09-12 01:11:13 +0530

    Update Week 1 assignment solutions

--- SeatDuplicationChecker.java
+++ SeatDuplicationChecker.java
@@ -1,18 +1,12 @@
 public class SeatDuplicationChecker {
 
     public static void checkDuplicateSeats(int[] seatNumbers) {
-
         boolean found = false;
 
         for (int i = 0; i < seatNumbers.length; i++) {
-
             for (int j = i + 1; j < seatNumbers.length; j++) {
-
                 if (seatNumbers[i] == seatNumbers[j]) {
-
-                    System.out.println("Duplicate Seat Number Found: "
-                            + seatNumbers[i]);
-
+                    System.out.println("Duplicate Seat Number Found: " + seatNumbers[i]);
                     found = true;
                 }
             }
@@ -24,9 +18,7 @@ public class SeatDuplicationChecker {
     }
 
     public static void main(String[] args) {
-
-        int[] seats = {101, 102, 103, 102, 105};
-
-        checkDuplicateSeats(seats);
+        int[] seatNumbers = {101, 102, 103, 102, 105};
+        checkDuplicateSeats(seatNumbers);
     }
 }
