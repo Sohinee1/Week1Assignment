1372caf889147d4e6d6dc10d1db5e46b5ce11da7
Sohinee1 <sc5248@srmist.edu.in>
2026-09-12 00:01:10 +0530

    Add Week 1 practice Problem 05

---
+++ Class_Practice_Problems/Problem05.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Problem05 {
+
+    static String reverseCustomerName(String customerName) {
+        char[] characters = customerName.toCharArray();
+        String reversed = "";
+
+        for (int i = characters.length - 1; i >= 0; i--) {
+            reversed += characters[i];
+        }
+
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter customer name: ");
+        String customerName = sc.nextLine();
+
+        System.out.println("Original Name: " + customerName);
+        System.out.println("Reversed Name: " + reverseCustomerName(customerName));
+    }
+}
