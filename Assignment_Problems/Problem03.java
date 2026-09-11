9abe9c7ef117120cf5cc37dd2d081f5e66da5bfd
Sohinee1 <sc5248@srmist.edu.in>
2026-09-12 00:01:23 +0530

    Organize Week 1 assignment Problem 03

---
+++ Assignment_Problems/Problem03.java
@@ -0,0 +1,28 @@
+public class Problem03 {
+
+    public static void findLongestStreak(String signalLog) {
+        int maxCount = 1;
+        int currentCount = 1;
+        char maxChar = signalLog.charAt(0);
+
+        for (int i = 1; i < signalLog.length(); i++) {
+            if (signalLog.charAt(i) == signalLog.charAt(i - 1)) {
+                currentCount++;
+            } else {
+                currentCount = 1;
+            }
+
+            if (currentCount > maxCount) {
+                maxCount = currentCount;
+                maxChar = signalLog.charAt(i);
+            }
+        }
+
+        System.out.println("Longest Streak: " + maxChar + " repeated " + maxCount + " times");
+    }
+
+    public static void main(String[] args) {
+        String signal = "RRGGGYRR";
+        findLongestStreak(signal);
+    }
+}
